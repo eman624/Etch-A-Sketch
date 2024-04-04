3b65a6798367851190be0328e6ac131d17f70569
Edgar <edgar.noel.m@gmail.com>
2024-04-04 00:17:50 -0700

    backup without grid resetting

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-let gridSize = 0;
-
-const container = document.querySelector(".container");
-
-for (var col = 1; col <= 16; col++) {
-  const column = document.createElement("div");
-  column.classList.add("col");
-  for (var box = 1; box <= 16; box++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    column.appendChild(box);
-  }
-  container.appendChild(column);
-}
-
-const boxes = document.querySelectorAll(".box");
-
-boxes.forEach((box) => {
-  box.addEventListener("mouseover", () =>
-    box.setAttribute("style", "background:green")
-  );
-
-  box.addEventListener("mouseout", () =>
-    box.setAttribute("style", "background: blue")
-  );
-});
-
 const button = document.querySelector("button");
+
 button.addEventListener("click", () => {
-  gridSize = prompt("enter number");
+  let gridSize = 0;
+  do {
+    gridSize = prompt("enter number less than 100");
+  } while (gridSize > 100);
+
+  const container = document.querySelector(".container");
+
+  for (var col = 1; col <= gridSize; col++) {
+    const column = document.createElement("div");
+    column.classList.add("col");
+    for (var box = 1; box <= gridSize; box++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      column.appendChild(box);
+    }
+    container.appendChild(column);
+  }
+
+  const boxes = document.querySelectorAll(".box");
+
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", () =>
+      box.setAttribute("style", "background:green")
+    );
+
+    box.addEventListener("mouseout", () =>
+      box.setAttribute("style", "background: blue")
+    );
+  });
 });

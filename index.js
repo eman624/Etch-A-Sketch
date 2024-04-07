2d46ce180f62e23f616894f5bf2b68e713796283
Edgar <edgar.noel.m@gmail.com>
2024-04-07 12:58:05 -0700

    revert

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 const button = document.querySelector("button");
 
+const container = document.querySelector(".container");
+
+for (var col = 1; col <= getGridSize(); col++) {
+  const column = document.createElement("div");
+  column.classList.add("col");
+  for (var box = 1; box <= getGridSize(); box++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    column.appendChild(box);
+  }
+  container.appendChild(column);
+}
+
+console.log(getGridSize());
+
 button.addEventListener("click", () => {
   let gridSize = 0;
   do {
     gridSize = prompt("enter number less than 100");
   } while (gridSize > 100);
+});
 
-  const container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
-  for (var col = 1; col <= gridSize; col++) {
-    const column = document.createElement("div");
-    column.classList.add("col");
-    for (var box = 1; box <= gridSize; box++) {
-      const box = document.createElement("div");
-      box.classList.add("box");
-      column.appendChild(box);
-    }
-    container.appendChild(column);
+for (var col = 1; col <= gridSize; col++) {
+  const column = document.createElement("div");
+  column.classList.add("col");
+  for (var box = 1; box <= gridSize; box++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    column.appendChild(box);
   }
+  container.appendChild(column);
+}
 
-  const boxes = document.querySelectorAll(".box");
+const boxes = document.querySelectorAll(".box");
 
-  boxes.forEach((box) => {
-    box.addEventListener("mouseover", () =>
-      box.setAttribute("style", "background:green")
-    );
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", () =>
+    box.setAttribute("style", "background:green")
+  );
 
-    box.addEventListener("mouseout", () =>
-      box.setAttribute("style", "background: blue")
-    );
-  });
+  box.addEventListener("mouseout", () =>
+    box.setAttribute("style", "background: blue")
+  );
 });

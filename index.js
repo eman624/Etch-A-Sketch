3f9f04fd66759d76afdc4cd3ed49871ac5cba9c0
Edgar <edgar.noel.m@gmail.com>
2024-04-07 18:27:18 -0700

    rewriting code

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
-const button = document.querySelector("button");
+const container = document.querySelector(".container");
 
-button.addEventListener("click", () => {
-  let gridSize = 0;
-  do {
-    gridSize = prompt("enter number less than 100");
-  } while (gridSize > 100);
+function createGrid(gridSize) {
+  for (var i = 1; i <= gridSize; i++) {
+    const row = document.createElement("div");
+    row.classList.add("grid-row");
 
-  const container = document.querySelector(".container");
+    for (var j = 1; j <= gridSize; j++) {
+      const widthAndHeight = 1000 / gridSize;
+      const gridBox = document.createElement("div");
+      gridBox.classList.add("grid-box");
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
 
-  for (var col = 1; col <= gridSize; col++) {
-    const column = document.createElement("div");
-    column.classList.add("col");
-    for (var box = 1; box <= gridSize; box++) {
-      const box = document.createElement("div");
-      box.classList.add("box");
-      column.appendChild(box);
+      gridBox.addEventListener("mouseover", () => {
+        gridBox.style.background = "green";
+      });
+
+      gridBox.addEventListener("mouseout", () => {
+        gridBox.style.background = "blue";
+      });
+
+      row.appendChild(gridBox);
     }
-    container.appendChild(column);
+    container.appendChild(row);
   }
+}
 
-  const boxes = document.querySelectorAll(".box");
+createGrid(10);
 
-  boxes.forEach((box) => {
-    box.addEventListener("mouseover", () =>
-      box.setAttribute("style", "background:green")
-    );
+/* ******************************************************** */
 
-    box.addEventListener("mouseout", () =>
-      box.setAttribute("style", "background: blue")
-    );
-  });
-});
+// const button = document.querySelector("button");
+
+// button.addEventListener("click", () => {
+//   let gridSize = 0;
+//   do {
+//     gridSize = prompt("enter number less than 100");
+//   } while (gridSize > 100);
+
+//   const container = document.querySelector(".container");
+
+//   for (var col = 1; col <= gridSize; col++) {
+//     const column = document.createElement("div");
+//     column.classList.add("col");
+//     for (var box = 1; box <= gridSize; box++) {
+//       const box = document.createElement("div");
+//       box.classList.add("box");
+//       column.appendChild(box);
+//     }
+//     container.appendChild(column);
+//   }
+
+//   const boxes = document.querySelectorAll(".box");
+
+//   boxes.forEach((box) => {
+//     box.addEventListener("mouseover", () =>
+//       box.setAttribute("style", "background:green")
+//     );
+
+//     box.addEventListener("mouseout", () =>
+//       box.setAttribute("style", "background: blue")
+//     );
+//   });
+// });

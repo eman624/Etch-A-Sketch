f51a81b8882e020ce915af4cea121519c883ed41
Edgar <edgar.noel.m@gmail.com>
2024-04-13 21:15:34 -0700

    reset.button event lister creates new grids

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const container = document.querySelector(".container");
 const resetButton = document.querySelector(".reset-button");
 
 function createGrid(gridSize) {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("wrapper");
+
   for (var i = 1; i <= gridSize; i++) {
     const row = document.createElement("div");
     row.classList.add("grid-row");
@@ -23,17 +26,25 @@ function createGrid(gridSize) {
 
       row.appendChild(gridBox);
     }
-    container.appendChild(row);
+    wrapper.appendChild(row);
   }
+  container.appendChild(wrapper);
 }
 
 resetButton.addEventListener("click", () => {
   let newGridSize = 0;
   do {
     newGridSize = Number(prompt("enter number less than 100"));
-  } while (newGridSize > 100 || newGridSize < 0);
+  } while (newGridSize > 100);
 
-  createGrid(newGridSize);
+  const wrapper = document.querySelector(".wrapper");
+
+  if (!wrapper) {
+    createGrid(newGridSize);
+  } else {
+    wrapper.remove();
+    createGrid(newGridSize);
+  }
 });
 
 /* ******************************************************** */
